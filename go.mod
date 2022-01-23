85620540d26febdcb7e128a9de0d6931b40acd6d
Josh Curl <josh@curl.me>
2022-01-22 21:26:31 -0800

    Move to Go modules

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/hightouchio/injecto
+
+go 1.14
+
+require (
+	github.com/Microsoft/hcsshim v0.9.2 // indirect
+	github.com/containerd/containerd v1.5.9 // indirect
+	github.com/docker/docker v20.10.12+incompatible
+	github.com/moby/sys/mount v0.3.0 // indirect
+	github.com/opencontainers/runc v1.1.0 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	google.golang.org/grpc v1.43.0 // indirect
+)
--- vendor/github.com/docker/docker/pkg/system/stat_solaris.go
+++ vendor/github.com/docker/docker/pkg/system/stat_solaris.go
@@ -1,34 +1,13 @@
-// +build solaris
+package system // import "github.com/docker/docker/pkg/system"
 
-package system
+import "syscall"
 
-import (
-	"syscall"
-)
-
-// fromStatT creates a system.StatT type from a syscall.Stat_t type
+// fromStatT converts a syscall.Stat_t type to a system.Stat_t type
 func fromStatT(s *syscall.Stat_t) (*StatT, error) {
 	return &StatT{size: s.Size,
-		mode: uint32(s.Mode),
+		mode: s.Mode,
 		uid:  s.Uid,
 		gid:  s.Gid,
-		rdev: uint64(s.Rdev),
+		rdev: s.Rdev,
 		mtim: s.Mtim}, nil
-}
-
-// FromStatT loads a system.StatT from a syscal.Stat_t.
-func FromStatT(s *syscall.Stat_t) (*StatT, error) {
-	return fromStatT(s)
-}
-
-// Stat takes a path to a file and returns
-// a system.StatT type pertaining to that file.
-//
-// Throws an error if the file does not exist
-func Stat(path string) (*StatT, error) {
-	s := &syscall.Stat_t{}
-	if err := syscall.Stat(path, s); err != nil {
-		return nil, err
-	}
-	return fromStatT(s)
 }
